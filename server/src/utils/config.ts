d7dab5eb66987dc628d98443effda5ef8a83eb7a
meltedhyperion <aryan12official34@gmail.com>
2023-04-17 22:22:12 +0530

    add env config

---
+++ server/src/utils/config.ts
@@ -0,0 +1,12 @@
+import "dotenv/config";
+
+const port = process.env.PORT || 3000;
+const dbUri = process.env.MONGODB_URI || "";
+const dbName = process.env.DB_NAME || "blockchain";
+const authToken = process.env.TOKEN;
+
+if (!dbUri) {
+  throw new Error("Requires Database URI in .env");
+}
+
+export { port, dbUri, dbName, authToken };
