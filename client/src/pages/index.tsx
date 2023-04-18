ba28f91eea21a1675fd3eaa745bbee476254aea2
Mohamed Sami <srmf2025@gmail.com>
2023-04-18 18:02:41 +0530

    add tagline

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -12,7 +12,8 @@ function Page() {
 
   return (
     <div className="flex flex-col gap-3 p-16">
-      <h1 className="text-7xl font-bold">Tagline</h1>
+      <h1 className="text-7xl font-bold">De-Gilocker</h1>
+      <h2 className="text-2xl">Empower your data, at your fingertips</h2>
       <h2 className="text-4xl font-semibold">Connect Your Wallet to Get Started</h2>
     </div>
   )
